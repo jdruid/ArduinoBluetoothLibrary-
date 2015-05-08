adae9b37ef4bdc36c2e441d51ceec56922c1cc7f
jdruid <joshdrew@gmail.com>
2015-05-08 14:33:46 -0400

    Inital Commit

---
+++ ArduinoBluetoothLibrary/Converter/BluetoothStateConverter.cs
@@ -0,0 +1,43 @@
+﻿// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF 
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A 
+// PARTICULAR PURPOSE. 
+// 
+// Copyright (c) Microsoft Corporation. All rights reserved 
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+using ArduinoBluetooth.Connection; //BluetoothConnectionState
+
+// Code used from Bluetooth communication between Arduino and Windows 8.1
+// Author: Michael Osthege
+// http://code.msdn.microsoft.com/Bluetooth-communication-7130c260/sourcecode?fileId=97047&pathId=464830490
+
+namespace ArduinoBluetooth.Converter
+{
+    public class BluetoothStateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string culture)
+        {
+            switch (parameter as string)//parameter tells us who's calling
+            {
+                //BluetoothConnectionState
+                case "BluetoothConnect": return ((BluetoothConnectionState)value == BluetoothConnectionState.Disconnected);
+                case "BluetoothInProgress":
+                case "BluetoothConnecting": return ((BluetoothConnectionState)value == BluetoothConnectionState.Connecting);
+                case "BluetoothDisconnect": return ((BluetoothConnectionState)value == BluetoothConnectionState.Connected);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string culture)
+        {
+            return null;
+        }
+    }
+}
+
+
---
+++ ArduinoBluetoothLibrary/PropertyChangedBase.cs
@@ -0,0 +1,26 @@
+﻿// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF 
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A 
+// PARTICULAR PURPOSE. 
+// 
+// Copyright (c) Microsoft Corporation. All rights reserved 
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+// Code used from Bluetooth communication between Arduino and Windows 8.1
+// Author: Michael Osthege
+// http://code.msdn.microsoft.com/Bluetooth-communication-7130c260/sourcecode?fileId=97047&pathId=464830490
+
+namespace ArduinoBluetooth
+{
+    public class PropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) //Attention: usually: protected void
+        {
+            var eventHandler = this.PropertyChanged;
+            if (eventHandler != null)
+                eventHandler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
